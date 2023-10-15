3a5eb28b3d2d49c84236cd9221f90fddcf147e1a
Derlie Monne <derlie.monne@gmail.com>
2023-10-16 00:37:00 +1000

    add(grid): grid shader

---
+++ Assets/Grid/Grid.hlsl
@@ -0,0 +1,57 @@
+﻿#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+
+float _GridThickness;
+float _CellSize;
+float2 _GridXInWorldSpace;
+float2 _GridYInWorldSpace;
+bool _DebugShowGridSpaceCoordinates;
+
+struct Attributes
+{
+    float4 positionOS : POSITION;                 
+};
+
+struct Interpolators
+{
+    float4 positionCS : SV_POSITION;
+    float3 positionWS : TEXCOORD0;
+};            
+
+Interpolators Vert(Attributes input)
+{
+    Interpolators output;
+    VertexPositionInputs positionInputs = GetVertexPositionInputs(input.positionOS);
+    output.positionCS = TransformWorldToHClip(positionInputs.positionWS);
+    output.positionWS = positionInputs.positionWS;
+    return output;
+}
+
+half4 Frag(Interpolators input) : SV_Target
+{
+    float3 positionWS = input.positionWS;
+    float gridSpaceDeterminant = _GridXInWorldSpace.x * _GridYInWorldSpace.y - _GridYInWorldSpace.x * _GridXInWorldSpace.y;
+    float2x2 gridSpaceMatrixInverse = {
+        _GridYInWorldSpace.y, -_GridYInWorldSpace.x,
+        -_GridXInWorldSpace.y, _GridXInWorldSpace.x
+    };
+    gridSpaceMatrixInverse /= gridSpaceDeterminant;
+    float2 positionGS = mul(gridSpaceMatrixInverse, positionWS.xy);
+
+    half4 outputColor;
+    float xDistanceToGridLineGS = min(frac(positionGS.x), abs(1 - frac(positionGS.x)));
+    float yDistanceToGridLineGS = min(frac(positionGS.y), abs(1 - frac(positionGS.y)));
+    if (min(xDistanceToGridLineGS, yDistanceToGridLineGS) <= _GridThickness)
+    {
+        outputColor = half4(1, 1, 1, 1);;
+    }
+    else if (_DebugShowGridSpaceCoordinates)
+    {
+        outputColor = float4(positionGS, 0, 1);
+    }
+    else
+    {
+        outputColor = half4(0, 0, 0, 0);
+    }
+    return outputColor;
+}
---
+++ Assets/Grid/Grid.shader
@@ -0,0 +1,39 @@
+﻿Shader "Sprites/Default"
+{
+    Properties
+    {
+        [MainTexture] _MainTex ("Main Texture", 2D) = "white" {}
+        _GridThickness ("Grid Thickness", Float) = 0.1
+        _CellSize ("Cell Size", Float) = 1
+        _GridXInWorldSpace ("Grid X In World Space", Vector) = (1, 0, 0, 0)
+        _GridYInWorldSpace ("Grid Y In World Space", Vector) = (0, 1, 0, 0)
+        [Toggle] _DebugShowGridSpaceCoordinates ("Debug Show Grid Space Coordinates", Float) = 0
+    }
+
+    SubShader
+    {
+        Tags
+        {
+            "Queue"="Transparent"
+            "IgnoreProjector"="True"
+            "RenderType"="Transparent"
+            "PreviewType"="Plane"
+            "CanUseSpriteAtlas"="True"
+            "RenderPipeline" = "UniversalPipeline"
+        }
+
+        Cull Off
+        Lighting Off
+        ZWrite Off
+        Blend One OneMinusSrcAlpha
+
+        Pass
+        {
+        HLSLPROGRAM
+            #include "Grid.hlsl"
+            #pragma vertex Vert
+            #pragma fragment Frag
+        ENDHLSL
+        }
+    }
+}
