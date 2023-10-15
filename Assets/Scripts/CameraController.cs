a06d4c4d203677b4749b8a74cd0e0af54d7196dc
Derlie Monne <derlie.monne@gmail.com>
2023-10-16 00:35:42 +1000

    add(camera): camera controller script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// Attach to Main Camera game object.
+/// </summary>
+public class CameraController : MonoBehaviour
+{
+    /// <summary>
+    /// 0 - left mouse button, 1 - right mouse button.
+    /// </summary>
+    public int MouseButtonToPan = 0;
+    public float PanSensitivity = 1f;
+    public float MinPanWorldSpace = 0.3f;
+    public float InterpolationMultiplier = 10f;
+    public Camera Camera;
+
+    private Vector2? _cameraStartPosition;
+    private Vector2? _startPanPositionScreenSpace;
+    private Vector2? _cameraDesiredPosition;
+
+    private void Awake()
+    {
+        Camera = GetComponent<Camera>();
+        if (Camera == null)
+        {
+            Debug.LogError("This script must be attached to Camera gameobject. No camera found on this gameobject.");
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(MouseButtonToPan))
+        {
+            _startPanPositionScreenSpace = Input.mousePosition;
+            _cameraStartPosition = transform.position;
+        }
+        else if (Input.GetMouseButton(MouseButtonToPan) &&
+            _startPanPositionScreenSpace.HasValue &&
+            _cameraStartPosition.HasValue)
+        {
+            Vector2 delta = Camera.ScreenToWorldPoint(Input.mousePosition) - Camera.ScreenToWorldPoint(_startPanPositionScreenSpace.Value);
+            if (delta.sqrMagnitude < MinPanWorldSpace * MinPanWorldSpace)
+            {
+                delta = Vector2.zero;
+            }
+            Vector2 newPosition = _cameraStartPosition.Value + (-delta * PanSensitivity);
+            _cameraDesiredPosition = newPosition;
+        }
+        else if (Input.GetMouseButtonUp(MouseButtonToPan))
+        {
+            _startPanPositionScreenSpace = null;
+            _cameraStartPosition = null;
+        }
+
+        if (_cameraDesiredPosition.HasValue)
+        {
+            if (InterpolationMultiplier < 1e-5)
+            {
+                Debug.LogWarning("InterpolationMultiplier is zero. Camera pan won't work.");
+            } 
+            Vector3 newPosition = Vector2.Lerp(transform.position, _cameraDesiredPosition.Value, Time.deltaTime * InterpolationMultiplier);
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
+        
+    }
+}
